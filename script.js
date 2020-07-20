691921c2e3153811c7e92ae055e0971a6f5bab7f
Hamza <hamza.husein017@gmail.com>
2020-07-20 09:29:16 +0530

    get votes for each image

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 //API key: 98edebaa-a6fb-4fe5-b876-e0d9e18f1bbe
 
 const api_url="https://api.thecatapi.com/v1/images/search";
+const vote_url="https://api.thecatapi.com/v1/votes";
 
 const apiKey = '98edebaa-a6fb-4fe5-b876-e0d9e18f1bbe';
 
@@ -11,36 +12,47 @@ let vote_id;
 
 const sendHTTPRequest = (method, url, data) =>{
     const promise = new Promise((resolve,reject) =>{
+
         const xhr = new XMLHttpRequest();
+
         xhr.open(method, url); //prepares a GET request package
 
         xhr.responseType = 'json'; //parses JSON into readable JS object
 
         if(data){
             xhr.setRequestHeader('Content-Type', 'application/json'); //to make data into JSON for server
-            xhr.setRequestHeader('x-api-key', apiKey);
         }
         xhr.onload = () =>{
             resolve(xhr.response);
         }
 
+        xhr.setRequestHeader('x-api-key', apiKey);
         xhr.send(JSON.stringify(data)); //sends the package
+
     });
+
     return promise
     
 };
 
 
 const voteUp = () =>{
-    sendHTTPRequest('POST', 'https://api.thecatapi.com/v1/votes', {'image_id': $('#img'), 'value': 1 })
+    console.log($('img').attr('id'));
+    sendHTTPRequest('POST', 'https://api.thecatapi.com/v1/votes', {'image_id': $('img').attr('id'), 'value': 1 })
+    getVotes();
 }
 
 const voteDown = () =>{
-    sendHTTPRequest('POST', 'https://api.thecatapi.com/v1/votes', {'image_id': $('#img'), 'value': 0 })
+    console.log($('img').attr('id'));
+    sendHTTPRequest('POST', 'https://api.thecatapi.com/v1/votes', {'image_id': $('img').attr('id'), 'value': 0 })
+    getVotes();
 }
 
-
-
+const getVotes = () =>{
+    sendHTTPRequest('GET', vote_url).then(response =>{
+        console.log(response);
+    });
+}
 
 const getImages = () =>{
     sendHTTPRequest('GET', api_url).then(response =>{
